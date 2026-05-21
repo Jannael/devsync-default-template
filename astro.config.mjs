21b0426b97293f6589f00a796cb59aaa3a5e6949
Jannael Orlando <orlandojannael@gmail.com>
2026-05-20 18:40:09 -0600

    fix: styles not in output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import { defaultLang, languages } from './src/devsync/devsync'
+import devsync, { defaultLang, languages } from './src/devsync/devsync'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: devsync.site,
   i18n: {
     defaultLocale: defaultLang,
     locales: languages,
