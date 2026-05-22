27ae179332f6d16cf94fa732fd7765f67533ce94
Jannael Orlando <orlandojannael@gmail.com>
2026-05-22 14:52:00 -0600

    fix: no cloudfare adapter to deploy as static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import devsync, { defaultLang, languages } from './src/devsync/devsync'
-import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    prerenderEnvironment: 'node'
-  }),
   vite: {
     plugins: [tailwindcss()],
   },
