f8b1c88c6e7e90e864e5d4011e1a961f76d39130
Jannael Orlando <orlandojannael@gmail.com>
2026-05-23 15:36:35 -0600

    feat: add @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import devsync, { defaultLang, languages } from './src/devsync/devsync'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: devsync.site,
+
   i18n: {
     defaultLocale: defaultLang,
     locales: languages,
@@ -16,4 +20,6 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+
+  integrations: [sitemap()],
 })
